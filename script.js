589a9e7df8c0f1a033aa3c745b182c601e4df63f
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-28 08:39:24 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+document.getElementById("userform").addEventListener("submit",(event)=>{
+	event.preventDefault();
+
+	const name=document.getElementListener("name").value.trim();
+	const age=document.getElementListener("age").value.trim();
+
+	if(!name||!age){
+		alert("please fill out the all fields.");
+		return;
+	}
+	handleFormSubmission(name,parseInt(age));
+	
+});
+
+function handleFormSubmission(name,age){
+	const promise = new promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>=18){
+	           resolve(`Welcome, <name>. You can vote.`);
+			}else{
+				resolve(`Oh sorry <name>. You aren't old enough.`);
+			}
+		},4000);
+	});
+	promise.then((message)=>{
+		alert(message);
+	});
+	.catch((error){
+		alert(err)
+	});
+}
